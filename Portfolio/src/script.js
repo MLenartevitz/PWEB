b38467b8fde3e2f738a4acc2cd1f2a8001071d16
Mikael Lenartevitz <110952283+MLenartevitz@users.noreply.github.com>
2023-10-22 20:27:06 -0300

    Delete Portfolio directory

--- Portfolio/src/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav ul li a').forEach(item => {
-    item.addEventListener('click', function () {
-        document.querySelector('nav ul li a.ativo').classList.remove('ativo');
-        this.classList.add('ativo');
-    });
-});
-
